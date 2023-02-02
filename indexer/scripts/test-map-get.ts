901a806121029c283865b531e00cc5ba72df058a
Hank Stoever <hstove@gmail.com>
2023-02-02 12:24:51 -0800

    fix: fetching primary name id

---
+++ indexer/scripts/test-map-get.ts
@@ -0,0 +1,24 @@
+import { StacksMainnet } from "micro-stacks/network";
+import { registryContract } from "../src/contracts";
+import { fetchMapGet } from "@clarigen/core";
+
+async function run() {
+  const network = new StacksMainnet();
+  const registry = registryContract();
+
+  const res = await fetchMapGet(
+    registry.identifier,
+    registry.maps.ownerPrimaryNameMap,
+    "SP1JTCR202ECC6333N7ZXD7MK7E3ZTEEE1MJ73C60",
+    {
+      network,
+    }
+  );
+  console.log(res);
+}
+
+run()
+  .catch(console.error)
+  .finally(() => {
+    process.exit();
+  });
