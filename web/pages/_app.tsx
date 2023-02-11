769ec14b65b17527797396cb4ccc16d22276e2d8
Hank Stoever <hstove@gmail.com>
2023-02-10 16:23:09 -0800

    fix: app name

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -10,7 +10,7 @@ import { queryClient } from '@store/query-client';
 import type { AppProps } from 'next/app';
 import type { ClientConfig } from '@micro-stacks/client';
 import { useRouter } from 'next/router';
-import { getNetwork, getAppUrl } from '@common/constants';
+import { getNetwork, getAppUrl, ONLY_INSCRIPTIONS } from '@common/constants';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -32,7 +32,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ClientProvider
-      appName="Dots"
+      appName={ONLY_INSCRIPTIONS ? 'BNS' : 'Dots'}
       appIconUrl={`${appUrl}/logo.svg`}
       dehydratedState={pageProps?.dehydratedState}
       onPersistState={onPersistState}
