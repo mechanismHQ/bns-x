76e75c42081c098c09aa32a49c711fbd59884175
Hank Stoever <hstove@gmail.com>
2023-02-11 18:00:08 -0800

    feat: card meta

--- web/components/head.tsx
+++ web/components/head.tsx
@@ -3,6 +3,7 @@ import NextHead from 'next/head';
 import { useAtomValue, useUpdateAtom } from 'jotai/utils';
 import { getAppUrl } from '../common/constants';
 import { pageTitleState, docTitleState, pageDescriptionState } from '@store';
+import { useRouter } from 'next/router';
 
 export const useSetTitle = (title: string) => {
   const setTitle = useUpdateAtom(docTitleState);
@@ -14,15 +15,28 @@ export const useSetTitle = (title: string) => {
 export const Head: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   const titleText = useAtomValue(pageTitleState);
   const description = useAtomValue(pageDescriptionState);
+  const router = useRouter();
+  const appUrl = getAppUrl();
   const icon = useMemo(() => {
-    return getAppUrl() + '/logo.svg';
-  }, []);
+    return appUrl + '/logo.svg';
+  }, [appUrl]);
+
+  const url = useMemo(() => {
+    return new URL(appUrl);
+  }, [appUrl]);
+
   return (
     <NextHead>
       <title>{titleText}</title>
       <link rel="icon" href={icon} type="image/svg+xml"></link>
       <meta property="og:title" content={titleText} />
       <meta property="og:description" content={description} />
+      <meta property="og:url" content={appUrl + router.pathname} />
+      <meta property="og:image" content={appUrl + '/card.png'} />
+      <meta property="twitter:card" content={'summary'} />
+      <meta property="twitter:url" content={appUrl + router.pathname} />
+      <meta property="twitter:image" content={appUrl + '/card.png'} />
+      <meta property="twitter:domain" content={url.host} />
       <meta property="twitter:title" content={titleText} />
       <meta property="twitter:description" content={description} />
       {children}
