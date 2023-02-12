32c810ac965f2e93b9837827b9cf33311a50cfd2
Hank Stoever <hstove@gmail.com>
2023-02-11 16:36:06 -0800

    feat: header/intro on nostr page

--- web/components/p/nostr.tsx
+++ web/components/p/nostr.tsx
@@ -4,6 +4,7 @@ import { Text } from '@components/text';
 import { allNostrNamesState, trpc } from '@store/api';
 import { useAtomValue } from 'jotai';
 import { ExternalLinkIcon } from '@components/icons/external-link';
+import { Link } from '@components/link';
 
 export const Nostr: React.FC = () => {
   const nostrNames = useAtomValue(allNostrNamesState);
@@ -11,7 +12,7 @@ export const Nostr: React.FC = () => {
   const rows = useMemo(() => {
     return nostrNames.map(name => {
       return (
-        <Stack key={name.name} spacing="3px" px="29px">
+        <Stack key={name.name} spacing="3px">
           <Stack isInline alignItems={'center'}>
             <Text variant="Heading035">{name.name}</Text>
             <ExternalLinkIcon
@@ -29,5 +30,18 @@ export const Nostr: React.FC = () => {
     });
   }, [nostrNames]);
 
-  return <Stack spacing="15px">{rows}</Stack>;
+  return (
+    <Stack spacing="20px" px="29px">
+      <Text variant="Heading02">Nostr on BNS</Text>
+      <Text variant="Body01" maxWidth="600px">
+        A directory of BNS users who have linked their Nostr public keys to their BNS names.
+        Interested in joining the list? Use{' '}
+        <Link href="https://btc.us" target="_blank">
+          btc.us
+        </Link>{' '}
+        to update your Zonefile.
+      </Text>
+      {rows}
+    </Stack>
+  );
 };
