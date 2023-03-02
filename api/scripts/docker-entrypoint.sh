c4b059f0ec345a1b0e98c4e50c2e9f2a13ee965b
Hank Stoever <hstove@gmail.com>
2023-03-02 12:15:11 -0800

    fix: connection pooling for db

--- api/scripts/docker-entrypoint.sh
+++ api/scripts/docker-entrypoint.sh
@@ -3,9 +3,18 @@
 if [ "${DATABASE_URL}" ]; then
   if [[ "${DATABASE_URL}" == *\?* ]]
   then
-    export STACKS_API_POSTGRES="${DATABASE_URL}&schema=stacks_blockchain_api"
+    export STACKS_API_POSTGRES="${DATABASE_URL}&schema=stacks_blockchain_api&connection_limit=25&pool_timeout=15"
   else
-    export STACKS_API_POSTGRES="${DATABASE_URL}?schema=stacks_blockchain_api"
+    export STACKS_API_POSTGRES="${DATABASE_URL}?schema=stacks_blockchain_api&connection_limit=25&pool_timeout=15"
+  fi
+fi
+
+if [ "${BNSX_DB_URL}" ]; then
+  if [[ "${BNSX_DB_URL}" == *\?* ]]
+  then
+    export BNSX_DB_URL="${BNSX_DB_URL}&connection_limit=25&pool_timeout=15"
+  else
+    export BNSX_DB_URL="${BNSX_DB_URL}?connection_limit=25&pool_timeout=15"
   fi
   
 fi
