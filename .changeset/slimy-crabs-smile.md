b0affde08c221bb5836f644e462eb290e962d845
Hank Stoever <hstove@gmail.com>
2023-03-03 12:45:47 -0800

    feat: show connect popup when wallet not installed

---
+++ .changeset/slimy-crabs-smile.md
@@ -0,0 +1,5 @@
+---
+'web': patch
+---
+
+Fixed an issue where the confirmation screen could show 'pending' after sending a BNSx name to a different recipient.
--- web/components/wallet-connect-button.tsx
+++ web/components/wallet-connect-button.tsx
@@ -1,13 +1,40 @@
-import { useAuth } from '@micro-stacks/react';
+import { useAppDetails, useAuth } from '@micro-stacks/react';
 import { Button } from '@components/button';
+import { defineCustomElements } from '@stacks/connect-ui/loader';
+import { useCallback } from 'react';
 
 export const WalletConnectButton = () => {
   const { openAuthRequest, isRequestPending } = useAuth();
+  const appDetails = useAppDetails();
   const label = isRequestPending ? 'Loading...' : 'Connect wallet';
+
+  const showConnect = useCallback(async () => {
+    if ('StacksProvider' in window) {
+      await openAuthRequest();
+    } else {
+      await defineCustomElements(window);
+      const element = document.createElement('connect-modal');
+      element.authOptions = {
+        appDetails: {
+          name: appDetails.appName,
+          icon: appDetails.appIconUrl,
+        },
+      };
+      document.body.appendChild(element);
+      const handleEsc = (ev: KeyboardEvent) => {
+        if (ev.key === 'Escape') {
+          document.removeEventListener('keydown', handleEsc);
+          element.remove();
+        }
+      };
+      document.addEventListener('keydown', handleEsc);
+    }
+  }, []);
   return (
     <Button
       onClick={async () => {
-        await openAuthRequest();
+        await showConnect();
+        // await openAuthRequest();
       }}
     >
       {label}
