dd4af4bd0fdd4b5010a3722ddb043c176bc214aa
Hank Stoever <hstove@gmail.com>
2023-03-04 10:45:43 -0800

    fix: concurrency when syncing los

--- api/src/sync/prints.ts
+++ api/src/sync/prints.ts
@@ -7,6 +7,7 @@ import type { ContractLogs } from '../../prisma/generated/stacks-api-schema';
 import { cvToJSON } from '@clarigen/core';
 import { logger } from '~/logger';
 import { getDeployer } from '~/contracts';
+import PQueue from 'p-queue';
 
 type WhereInput = StacksDbTypes.ContractLogsWhereInput;
 
@@ -50,8 +51,19 @@ export async function syncPrints({
         {
           block_height: block_height,
           microblock_sequence: { gte: microblock_sequence },
+        },
+        {
+          block_height: block_height,
+          microblock_sequence,
           tx_index: { gte: tx_index },
-          event_index: { gt: event_index },
+        },
+        {
+          block_height: block_height,
+          microblock_sequence,
+          tx_index,
+          event_index: {
+            gte: tx_index,
+          },
         },
       ],
     };
@@ -93,9 +105,13 @@ export async function syncPrints({
   await syncPrints({ bnsDb, stacksDb, lastLog: lastSynced });
 }
 
+type MappedLog = ContractLogs & { json: BnsDbTypes.InputJsonValue; hex: Uint8Array };
+
+const logQueue = new PQueue({ concurrency: 10 });
+
 // Create or update "printEvent" table based on raw logs
 async function processLogs({ bnsDb, logs }: { bnsDb: BnsDb; logs: ContractLogs[] }) {
-  const mappedLogs: (ContractLogs & { json: BnsDbTypes.InputJsonValue; hex: Uint8Array })[] = [];
+  const mappedLogs: MappedLog[] = [];
 
   logs.forEach(log => {
     const hex = log.value;
@@ -110,75 +126,79 @@ async function processLogs({ bnsDb, logs }: { bnsDb: BnsDb; logs: ContractLogs[]
   });
 
   const syncs = mappedLogs.map(async contractLog => {
-    const baseProps: BnsDbTypes.PrintEventCreateInput = {
-      stacksApiId: contractLog.id,
-      microblockCanonical: contractLog.microblock_canonical,
-      canonical: contractLog.canonical,
-      microblockSequence: contractLog.microblock_sequence,
-      contractId: contractLog.contract_identifier,
-      value: contractLog.json,
-      hex: Buffer.from(contractLog.hex),
-      topic: contractLog.topic ?? '',
-      txIndex: contractLog.tx_index,
-      eventIndex: contractLog.event_index,
-      blockHeight: contractLog.block_height,
-      indexBlockHash: contractLog.index_block_hash,
-      microblockHash: contractLog.microblock_hash,
-      txid: contractLog.tx_id,
-    };
-    const existing = await bnsDb.printEvent.findFirst({
-      where: {
-        txid: contractLog.tx_id,
-        eventIndex: contractLog.event_index,
-      },
-    });
-    log.trace(
-      {
-        logBlockHeight: contractLog.block_height,
-      },
-      'Syncing log entry'
-    );
-    if (existing) {
-      log.trace(
-        {
-          blockHeight: contractLog.block_height,
-        },
-        'Updating existing'
-      );
-      await bnsDb.printEvent.updateMany({
-        where: {
-          txid: contractLog.tx_id,
-          eventIndex: contractLog.event_index,
-        },
-        data: {
-          canonical: contractLog.canonical,
-          microblockCanonical: contractLog.microblock_canonical,
-        },
-      });
-    } else {
-      log.trace(
-        {
-          print: contractLog.json,
-        },
-        'New contract log'
-      );
-      await bnsDb.printEvent.upsert({
-        where: {
-          blockHeight_microblockSequence_txIndex_eventIndex: {
-            blockHeight: contractLog.block_height,
-            microblockSequence: contractLog.microblock_sequence,
-            txIndex: contractLog.tx_index,
-            eventIndex: contractLog.event_index,
-          },
-        },
-        create: baseProps,
-        update: {
-          ...baseProps,
-        },
-      });
-    }
+    await logQueue.add(() => processLog(contractLog, bnsDb));
   });
 
   await Promise.all(syncs);
   return logs[logs.length - 1];
+}
+
+async function processLog(contractLog: MappedLog, bnsDb: BnsDb) {
+  const baseProps: BnsDbTypes.PrintEventCreateInput = {
+    stacksApiId: contractLog.id,
+    microblockCanonical: contractLog.microblock_canonical,
+    canonical: contractLog.canonical,
+    microblockSequence: contractLog.microblock_sequence,
+    contractId: contractLog.contract_identifier,
+    value: contractLog.json,
+    hex: Buffer.from(contractLog.hex),
+    topic: contractLog.topic ?? '',
+    txIndex: contractLog.tx_index,
+    eventIndex: contractLog.event_index,
+    blockHeight: contractLog.block_height,
+    indexBlockHash: contractLog.index_block_hash,
+    microblockHash: contractLog.microblock_hash,
+    txid: contractLog.tx_id,
+  };
+  const existing = await bnsDb.printEvent.findFirst({
+    where: {
+      txid: contractLog.tx_id,
+      eventIndex: contractLog.event_index,
+    },
+  });
+  log.trace(
+    {
+      logBlockHeight: contractLog.block_height,
+    },
+    'Syncing log entry'
+  );
+  if (existing) {
+    log.trace(
+      {
+        blockHeight: contractLog.block_height,
+      },
+      'Updating existing'
+    );
+    await bnsDb.printEvent.updateMany({
+      where: {
+        txid: contractLog.tx_id,
+        eventIndex: contractLog.event_index,
+      },
+      data: {
+        canonical: contractLog.canonical,
+        microblockCanonical: contractLog.microblock_canonical,
+      },
+    });
+  } else {
+    log.trace(
+      {
+        print: contractLog.json,
+      },
+      'New contract log'
+    );
+    await bnsDb.printEvent.upsert({
+      where: {
+        blockHeight_microblockSequence_txIndex_eventIndex: {
+          blockHeight: contractLog.block_height,
+          microblockSequence: contractLog.microblock_sequence,
+          txIndex: contractLog.tx_index,
+          eventIndex: contractLog.event_index,
+        },
+      },
+      create: baseProps,
+      update: {
+        ...baseProps,
+      },
+    });
+  }
 }
