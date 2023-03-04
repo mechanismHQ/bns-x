84159c3698cfa02910b469ddba2f93ecbadb5934
Hank Stoever <hstove@gmail.com>
2023-03-04 10:19:42 -0800

    fix: catch refresh materialized error

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -3,9 +3,17 @@ export { PrismaClient as StacksDb } from '../../prisma/generated/stacks-api-sche
 export type { Prisma as BnsDbTypes } from '@prisma/client';
 export type { Prisma as StacksDbTypes } from '../../prisma/generated/stacks-api-schema';
 import type { PrismaClient as BnsDb } from '@prisma/client';
+import { logger } from '~/logger';
 
 export async function refreshMaterializedViews(db: BnsDb) {
-  await db.$executeRaw`REFRESH MATERIALIZED VIEW CONCURRENTLY names`;
-  await db.$executeRaw`REFRESH MATERIALIZED VIEW CONCURRENTLY primary_names`;
-  await db.$executeRaw`REFRESH MATERIALIZED VIEW CONCURRENTLY name_ownership`;
+  try {
+    await db.$executeRaw`REFRESH MATERIALIZED VIEW CONCURRENTLY names`;
+    await db.$executeRaw`REFRESH MATERIALIZED VIEW CONCURRENTLY primary_names`;
+    await db.$executeRaw`REFRESH MATERIALIZED VIEW CONCURRENTLY name_ownership`;
+  } catch (error) {
+    logger.error({
+      error,
+      topic: 'refresh-materialized-views',
+    });
+  }
 }
