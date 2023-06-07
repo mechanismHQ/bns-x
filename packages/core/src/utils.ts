f5f87544e46c56b120d6d88b8c5d9569136f2a54
Ryan Waits <ryan.waits@gmail.com>
2023-06-07 16:03:36 -0500

    update hashFqn to use hash160 and match the hashFqn in tests module

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -2,6 +2,7 @@ import { bytesToAscii, hexToBytes, bytesToHex, asciiToBytes } from 'micro-stacks
 import type { WithCombined, NameBuff, NameBase, LegacyDetails, LegacyJson } from './types';
 import { toUnicode } from '@bns-x/punycode';
 import { hashRipemd160, getRandomBytes } from 'micro-stacks/crypto';
+import { createHash } from 'crypto';
 
 export function getContractParts(identifier: string): [string, string] {
   const [addr, name] = identifier.split('.');
@@ -93,16 +94,80 @@ export function randomSalt() {
   return getRandomBytes(20);
 }
 
-export function hashFqn(name: string, namespace: string, salt: string | Uint8Array) {
-  const saltHex = typeof salt === 'string' ? salt : bytesToHex(salt);
-  const nameBytes = asciiToHex(name);
-  const namespaceBytes = asciiToHex(namespace);
-  return hashRipemd160(hexToBytes(`${nameBytes}2e${namespaceBytes}${saltHex}`));
+/**
+ * Computes the RIPEMD-160 hash of the SHA-256 hash of a string.
+ * The input string is first hashed using the SHA-256 algorithm.
+ * The resulting SHA-256 hash is then hashed using the RIPEMD-160 algorithm.
+ * The final hash value is returned as a Uint8Array.
+ * @param data - The string to hash.
+ * @returns A Uint8Array containing the RIPEMD-160 hash of the SHA-256 hash of the input string.
+ */
+export function hash160(data: string) {
+  // Compute the SHA-256 hash of the input string.
+  const sha256 = createHash('sha256').update(data).digest();
+
+  // Compute the RIPEMD-160 hash of the SHA-256 hash.
+  const ripe160 = createHash('ripemd160').update(sha256).digest();
+
+  // Return the resulting hash value as a Uint8Array.
+  return new Uint8Array(ripe160);
 }
 
-export const NO_EXPIRATION_NAMESPACES = new Set(
-  ...['stx', 'app', 'stacks', 'podcast', 'miner', 'mining', 'helloworld', 'stacking', 'blockstack']
-);
+/**
+ * Converts a hexadecimal string to a regular string.
+ * Each pair of hexadecimal characters in the input string is converted to a Unicode character.
+ * @param hex - The hexadecimal string to convert to a regular string.
+ * @returns A regular string corresponding to the input hexadecimal string.
+ */
+export function hexToString(hex: any) {
+  let str = '';
+  for (let i = 0; i < hex.length; i += 2) {
+    // Convert each pair of hexadecimal characters to a Unicode character.
+    const charCode = parseInt(hex.substr(i, 2), 16);
+    str += String.fromCharCode(charCode);
+  }
+  // Return the resulting regular string.
+  return str;
+}
+
+/**
+ * Computes the hash of a fully qualified name (FQN) with a salt value.
+ * The FQN is constructed by concatenating the name, namespace, and salt values.
+ * The resulting FQN string is then hashed using the hash160 function.
+ * @param name - The name of the domain being hashed.
+ * @param namespace - The namespace of the domain being hashed.
+ * @param salt - The salt value to use in the hash computation.
+ * @returns A Uint8Array containing the hash value of the FQN with salt.
+ */
+export function hashFqn(name: string, namespace: string, salt: string | Uint8Array) {
+  // Convert the salt value to a hexadecimal string if it's not already in that format.
+  const saltHex = typeof salt === 'string' ? salt : bytesToHex(salt);
+
+  // Convert the name and namespace values to hexadecimal strings.
+  const nameBytes = asciiToHex(name);
+  const namespaceBytes = asciiToHex(namespace);
+
+  // Concatenate the name, namespace, and salt values to form the fully qualified name (FQN) with salt.
+  const fqnWithSalt = `${nameBytes}2e${namespaceBytes}${saltHex}`;
+
+  // Convert the FQN with salt from hexadecimal to a regular string.
+  const fqnWithSaltString = hexToString(fqnWithSalt);
+
+  // Compute the hash of the FQN with salt using the hash160 function.
+  return hash160(fqnWithSaltString);
+}
+
+export const NO_EXPIRATION_NAMESPACES = new Set([
+  'stx',
+  'app',
+  'stacks',
+  'podcast',
+  'miner',
+  'mining',
+  'helloworld',
+  'stacking',
+  'blockstack',
+]);
 
 /**
  * Helper function to expose namespaces that do not expire.
