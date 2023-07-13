903771e71636d8d61c5a8c1416178e403e791d19
Hank Stoever <hstove@gmail.com>
2023-07-13 11:45:46 -0700

    feat: setup shadcn-ui cli

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -14,7 +14,29 @@ export default {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
   },
   plugins: [tailwindAnimate],
 } satisfies Config;
