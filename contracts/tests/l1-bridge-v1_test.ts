1608374acba70101681b138731021eabdf391f1f
Hank Stoever <hstove@gmail.com>
2023-08-07 15:26:21 -0700

    feat: update bridge-v1 signature validation logic

--- contracts/tests/l1-bridge-v1_test.ts
+++ contracts/tests/l1-bridge-v1_test.ts
@@ -30,32 +30,36 @@ const contract = contracts.l1BridgeV1;
 const registry = contracts.l1Registry;
 const bnsx = contracts.bnsxRegistry;
 
+const namespace = btcBytes;
+
 describe('l1-bridge-v1', () => {
   const { chain } = deployWithNamespace();
 
-  function hashWrapData(nameId: bigint, header: Uint8Array, inscriptionId: Uint8Array) {
+  function hashWrapData(name: Uint8Array, namespace: Uint8Array, inscriptionId: Uint8Array) {
     return chain.rov(
       contract.hashWrapData({
-        headerHash: header,
+        name,
+        namespace,
         inscriptionId,
-        nameId,
       })
     );
   }
 
   function signWrap(
-    nameId: bigint,
-    header: Uint8Array,
+    name: Uint8Array,
+    namespace: Uint8Array,
+    // header: Uint8Array,
     inscriptionId: Uint8Array,
     signer: Uint8Array = deployerPK
   ) {
-    const hash = hashWrapData(nameId, header, inscriptionId);
+    const hash = hashWrapData(name, namespace, inscriptionId);
     const signature = signWithKey(hash, signer);
     return signature;
   }
 
   describe('successful wrap', () => {
     let height: bigint;
+    const name = asciiToBytes('alice');
     let nameId: bigint;
     let header: Uint8Array;
     let hash: Uint8Array;
@@ -75,13 +79,13 @@ describe('l1-bridge-v1', () => {
     });
 
     it('calls `wrap` method', () => {
-      const signature = signWrap(nameId, hash, inscriptionId);
+      const signature = signWrap(name, btcBytes, inscriptionId);
       const receipt = chain.txOk(
         contract.wrap({
-          nameId,
+          name,
+          namespace,
           inscriptionId,
           height,
-          headerHash: hash,
           signature,
         }),
         alice
@@ -113,6 +117,7 @@ describe('l1-bridge-v1', () => {
   describe('wrap validation', () => {
     const inscriptionId = randomBytes(33);
     let nameId: bigint;
+    const name = asciiToBytes('badsigner');
     beforeAll(() => {
       const regRes = utilsRegisterBtc({
         name: 'badsigner',
@@ -126,13 +131,14 @@ describe('l1-bridge-v1', () => {
       const height = BigInt(chain.blockHeight - 1);
       const hash = chain.rov(contract.hashForHeight(height));
 
-      const signature = signWrap(nameId, hash, inscriptionId, alicePK);
+      const signature = signWrap(name, namespace, inscriptionId, alicePK);
       const receipt = chain.txErr(
         contract.wrap({
-          nameId,
+          name,
+          namespace,
           inscriptionId,
           height,
-          headerHash: hash,
+          // headerHash: hash,
           signature,
         }),
         alice
@@ -141,36 +147,38 @@ describe('l1-bridge-v1', () => {
       assertEquals(receipt.value, contract.constants.ERR_INVALID_SIGNER.value);
     });
 
-    it('fails if invalid header hash at height', () => {
-      const height = BigInt(chain.blockHeight - 1);
-      // different height:
-      const hash = chain.rov(contract.hashForHeight(height - 1n));
-      const signature = signWrap(nameId, hash, inscriptionId, alicePK);
-      const receipt = chain.txErr(
-        contract.wrap({
-          nameId,
-          inscriptionId,
-          height,
-          headerHash: hash,
-          signature,
-        }),
-        alice
-      );
+    // it('fails if invalid header hash at height', () => {
+    //   const height = BigInt(chain.blockHeight - 1);
+    //   // different height:
+    //   const hash = chain.rov(contract.hashForHeight(height - 1n));
+    //   const signature = signWrap(name, namespace, inscriptionId, alicePK);
+    //   const receipt = chain.txErr(
+    //     contract.wrap({
+    //       name,
+    //       namespace,
+    //       inscriptionId,
+    //       height,
+    //       // headerHash: hash,
+    //       signature,
+    //     }),
+    //     alice
+    //   );
 
-      assertEquals(receipt.value, contract.constants.ERR_INVALID_BLOCK.value);
-    });
+    //   assertEquals(receipt.value, contract.constants.ERR_INVALID_BLOCK.value);
+    // });
 
     it('fails if sending other name than owned', () => {
       const height = BigInt(chain.blockHeight - 1);
       const hash = chain.rov(contract.hashForHeight(height));
 
-      const signature = signWrap(nameId, hash, inscriptionId);
+      const signature = signWrap(name, namespace, inscriptionId);
       const receipt = chain.txErr(
         contract.wrap({
-          nameId,
+          name,
+          namespace,
           inscriptionId,
           height,
-          headerHash: hash,
+          // headerHash: hash,
           signature,
         }),
         bob
