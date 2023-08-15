d4278e688b113cf01b9ce315388dea025ea1f36a
Hank Stoever <hstove@gmail.com>
2023-08-15 14:18:54 -0700

    feat: build script for ord template

--- packages/core/src/bridge/index.ts
+++ packages/core/src/bridge/index.ts
@@ -1,4 +1,11 @@
+import { ordCardTemplate } from './card';
+
+export const inscriptionJsSrc = '/content/TODO';
+
 export const inscriptionContentType = 'text/plain';
 export const inscriptionContentForName = (name: string) => {
-  return name;
+  let template = ordCardTemplate;
+  template = template.replace(/{{CARD_SRC}}/g, inscriptionJsSrc);
+  template = template.replace(/{{CARD_NAME}}/g, name);
+  return template;
 };
