ba1347294b76d6a7117380b1c3c8bb3411ed47af
Hank Stoever <hstove@gmail.com>
2023-08-22 12:23:54 -0700

    feat: tailwind-based Text component

--- web/tw-conf/design-token.js
+++ web/tw-conf/design-token.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Tue, 22 Aug 2023 17:23:47 GMT
+ * Generated on Tue, 22 Aug 2023 18:06:01 GMT
  */
 
 module.exports = {
@@ -193,7 +193,9 @@ module.exports = {
   "FontDisplayDisplay02": {"fontSize":64,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-1.28,"lineHeight":72,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"},
   "FontHeadingHeading01": {"fontSize":48,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.96,"lineHeight":60,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"},
   "FontHeadingHeading02": {"fontSize":40,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.8,"lineHeight":56,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"},
+  "FontHeadingHeading022": {"fontSize":22,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.64,"lineHeight":36,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"},
   "FontHeadingHeading03": {"fontSize":32,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.64,"lineHeight":44,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"},
+  "FontHeadingHeading035": {"fontSize":28,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.24,"lineHeight":44,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"},
   "FontHeadingHeading04": {"fontSize":24,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.24,"lineHeight":36,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"},
   "FontHeadingHeading05": {"fontSize":20,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.2,"lineHeight":28,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"},
   "FontHeadingHeading06": {"fontSize":18,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.18,"lineHeight":28,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"},
