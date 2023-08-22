41768b02e46674a8f1a73c5e06c436cc635f2e6a
Hank Stoever <hstove@gmail.com>
2023-08-22 10:41:48 -0700

    feat: integrate figma tokens in tailwind

---
+++ web/tw-conf/font-tokens.ts
@@ -0,0 +1,20 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 22 Aug 2023 17:23:47 GMT
+ */
+
+export const FontDisplayDisplay01 = {"fontSize":72,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-1.44,"lineHeight":80,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontDisplayDisplay02 = {"fontSize":64,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-1.28,"lineHeight":72,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontHeadingHeading01 = {"fontSize":48,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.96,"lineHeight":60,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontHeadingHeading02 = {"fontSize":40,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.8,"lineHeight":56,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontHeadingHeading03 = {"fontSize":32,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.64,"lineHeight":44,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontHeadingHeading04 = {"fontSize":24,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.24,"lineHeight":36,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontHeadingHeading05 = {"fontSize":20,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.2,"lineHeight":28,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontHeadingHeading06 = {"fontSize":18,"textDecoration":"none","fontFamily":"Open Sauce One","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.18,"lineHeight":28,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontLabelLabel01 = {"fontSize":16,"textDecoration":"none","fontFamily":"Inter","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.16,"lineHeight":24,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontLabelLabel02 = {"fontSize":14,"textDecoration":"none","fontFamily":"Inter","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.14,"lineHeight":20,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontLabelLabel03 = {"fontSize":12,"textDecoration":"none","fontFamily":"Inter","fontWeight":500,"fontStyle":"normal","fontStretch":"normal","letterSpacing":0,"lineHeight":16,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontBodyBody01 = {"fontSize":16,"textDecoration":"none","fontFamily":"Inter","fontWeight":400,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.16,"lineHeight":24,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontBodyBody02 = {"fontSize":14,"textDecoration":"none","fontFamily":"Inter","fontWeight":400,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.14,"lineHeight":20,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontCaptionCaption01 = {"fontSize":14,"textDecoration":"none","fontFamily":"Inter","fontWeight":400,"fontStyle":"normal","fontStretch":"normal","letterSpacing":-0.14,"lineHeight":20,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
+export const FontCaptionCaption02 = {"fontSize":12,"textDecoration":"none","fontFamily":"Inter","fontWeight":400,"fontStyle":"normal","fontStretch":"normal","letterSpacing":0,"lineHeight":16,"paragraphIndent":0,"paragraphSpacing":0,"textCase":"none"};
