c9aa3c77ad4fe11e8823d6ef4ac27827b9702cc1
Hank Stoever <hstove@gmail.com>
2023-08-24 13:16:29 -0700

    feat: layout tweaks

--- web/components/ui/text.tsx
+++ web/components/ui/text.tsx
@@ -3,21 +3,21 @@ import type { VariantProps } from 'class-variance-authority';
 import { cva } from 'class-variance-authority';
 import { cn } from '@common/utils';
 
-const textVariants = cva('text-text block', {
+const textVariants = cva('text-text block font-inter', {
   variants: {
     variant: {
       Body01: 'text-body01',
       Body02: 'text-body02',
       Caption01: 'text-text-subdued text-caption01',
       Caption02: 'text-text-subdued text-caption02',
-      Heading01: 'text-heading01',
-      Heading02: 'text-heading02',
-      Heading03: 'text-heading03',
-      Heading035: 'text-heading035',
-      Heading04: 'text-heading04',
-      Heading05: 'text-heading05',
-      Heading06: 'text-heading06',
-      Heading022: 'text-heading022',
+      Heading01: 'text-heading01 font-open-sauce',
+      Heading02: 'text-heading02 font-open-sauce',
+      Heading03: 'text-heading03 font-open-sauce',
+      Heading035: 'text-heading035 font-open-sauce',
+      Heading04: 'text-heading04 font-open-sauce',
+      Heading05: 'text-heading05 font-open-sauce',
+      Heading06: 'text-heading06 font-open-sauce',
+      Heading022: 'text-heading022 font-open-sauce',
       Display01: 'text-display01',
       Display02: 'text-display02',
     },
