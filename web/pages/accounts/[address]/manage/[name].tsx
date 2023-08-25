ad257046de449a1f903b226b85ad391d2786f04b
Hank Stoever <hstove@gmail.com>
2023-08-25 08:36:30 -0700

    feat: rename name management components

--- web/pages/accounts/[address]/manage/[name].tsx
+++ web/pages/accounts/[address]/manage/[name].tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import { Layout } from '@components/layout';
-import { Name } from '@components/p/name';
+import { ManageName } from '@components/p/manage';
 import { withSSRProps } from '@common/page-utils';
 
 export const getServerSideProps = withSSRProps();
@@ -8,7 +8,7 @@ export const getServerSideProps = withSSRProps();
 const NamePage: NextPage = () => {
   return (
     <Layout centerBox={false}>
-      <Name />
+      <ManageName />
     </Layout>
   );
 };
