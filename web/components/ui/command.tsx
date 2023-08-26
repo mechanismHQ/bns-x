33333ad7d0ba20760ea6e4463e52217267269d60
Hank Stoever <hstove@gmail.com>
2023-08-25 17:38:32 -0700

    feat: setup css vars for shadcn

--- web/components/ui/command.tsx
+++ web/components/ui/command.tsx
@@ -77,15 +77,6 @@ const CommandEmpty = React.forwardRef<
 
 CommandEmpty.displayName = CommandPrimitive.Empty.displayName;
 
-// const CommandLoading = React.forwardRef<
-//   React.ElementRef<typeof CommandPrimitive.Loading>,
-//   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Loading>
-// >(({ className, ...props }, ref) => (
-//   <CommandPrimitive.Loading ref={ref} className="py-6 text-center text-sm" {...props} />
-// ));
-
-// CommandLoading.displayName = CommandPrimitive.Loading.displayName;
-
 const CommandGroup = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Group>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Group>
