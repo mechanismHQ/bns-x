19a2685442db68280b76ec9c87751bd2b65e8da0
Hank Stoever <hstove@gmail.com>
2023-08-25 17:17:34 -0700

    feat: trpc endpoint to search names

--- api/src/fetchers/adapters/db-fetcher.ts
+++ api/src/fetchers/adapters/db-fetcher.ts
@@ -229,4 +229,57 @@ export class DbFetcher implements BaseFetcher {
 
     return result !== null;
   }
+
+  async searchNames(query: string) {
+    const fuzzyMatch = `%${query}%`;
+    const namespaceMatch = `${query}\.%`;
+    const preMatch = `${query}%`;
+    const results = await this.stacksDb.$queryRaw<{ name: string }[]>`
+      with fuzzy as (
+        select distinct on (name)
+        name
+        , 4 as rank
+        from names
+        where name like ${fuzzyMatch}
+        order by name
+      ), exact as (
+        select distinct on (name)
+        name
+        , 1 as rank
+        from names
+        where name = ${query}
+        order by name
+      ), starts as (
+        select distinct on (name)
+        name
+        , 3 as rank
+        from names
+        where name like ${preMatch}
+        order by name
+      ), pre as (
+        select distinct on (name)
+        name
+        , 2 as rank
+        from names
+        where name like ${namespaceMatch}
+        order by name
+      ), combined as (
+        select distinct on (fuzzy.name)
+        fuzzy.name
+        , least(fuzzy.rank, exact.rank, starts.rank, pre.rank) as rank
+        from fuzzy
+        left outer join exact on fuzzy.name = exact.name
+        left outer join starts on fuzzy.name = starts.name
+        left outer join pre on fuzzy.name = pre.name
+        order by name
+        , rank asc
+      )
+
+      select *
+      from combined
+      order by rank
+      limit 10;`;
+
+    return results.map(r => r.name);
+  }
 }
