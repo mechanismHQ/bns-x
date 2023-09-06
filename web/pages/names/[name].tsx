bbc35403ac6958516bee8b4679edeb14a2ea2b87
Hank Stoever <hstove@gmail.com>
2023-09-06 15:26:36 -0700

    fix: update name page when search changed

--- web/pages/names/[name].tsx
+++ web/pages/names/[name].tsx
@@ -4,6 +4,8 @@ import { NamePage } from '@components/p/name/name-page';
 import { withSSRProps } from '@common/page-utils';
 import { useHydrateAtoms } from 'jotai/utils';
 import { currentNameAtom } from '@store/names';
+import { useEffect } from 'react';
+import { useSetAtom } from 'jotai';
 
 export const getServerSideProps = withSSRProps(ctx => {
   return {
@@ -13,6 +15,10 @@ export const getServerSideProps = withSSRProps(ctx => {
 
 const Name: NextPage<{ name: string }> = ({ name }) => {
   useHydrateAtoms([[currentNameAtom, name]]);
+  const setName = useSetAtom(currentNameAtom);
+  useEffect(() => {
+    setName(name);
+  }, [name, setName]);
   return (
     <Layout centerBox={false}>
       <NamePage />
