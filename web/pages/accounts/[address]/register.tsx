949e30711a32499c861065b9edc5061c4c650d71
Hank Stoever <hstove@gmail.com>
2023-09-18 12:30:33 -0700

    fix: metadata for registration page

--- web/pages/accounts/[address]/register.tsx
+++ web/pages/accounts/[address]/register.tsx
@@ -3,7 +3,14 @@ import { Layout } from '@components/layout';
 import { Register } from '@components/p/register';
 import { withSSRProps } from '@common/page-utils';
 
-export const getServerSideProps = withSSRProps();
+export const getServerSideProps = withSSRProps(() => {
+  return {
+    meta: {
+      title: 'Register your BNS name',
+      description: 'Quickly and easily register your name on .btc, .stx, and many more.',
+    },
+  };
+});
 
 const RegisterPage: NextPage & { authRequired?: boolean } = () => {
   return (
